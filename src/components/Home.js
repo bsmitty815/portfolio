b8c2b246336b2818958f75e13c9dd9dc9b1d70c8
Brian Smith <brian.bubbam@gmail.com>
2021-10-20 14:29:54 -0400

    added some logos on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import LinkedinLogo from '../images/linkedin-logo.png'
 import GithubLogo from '../images/github-logo.png'
 import MediumLogo from '../images/medium-logo.png'
+import HtmlLogo from '../images/html-logo.png'
+import CssLogo from '../images/css-logo.png'
+import ReactLogo from '../images/react-logo.png'
+import RailsLogo from '../images/rails-logo.png'
+
 
 function Home() {
 
     return (
-        <div>
+        <div className="hom-container">
             <div className="row">
                 <div className="col-1-of-3">
                     <div className="home-links-container">
@@ -32,18 +37,28 @@ function Home() {
                 </div>
                 <div className="col-2-of-3">
                     <div className="home-right-box-container"> 
-
-
                         <h1>coming soon</h1>
-
-
                     </div>
-
-
-                    
+                </div>
+            </div>
+            <div >
+                <div class="row">
+                    <div class="col-1-of-4">
+                        <img src={HtmlLogo} alt="html" className="logo-links" />
+                    </div>
+                    <div class="col-1-of-4">
+                        <img src={ReactLogo} alt="react" className="logo-links" />
+                    </div>
+                    <div class="col-1-of-4">
+                        <img src={CssLogo} alt="css" className="logo-links" />
+                    </div>
+                    <div class="col-1-of-4">
+                        <img src={RailsLogo} alt="rails" className="logo-links" />
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
